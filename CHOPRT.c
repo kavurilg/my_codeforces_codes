43522e3d85d4384bce9353588c95625cbc3a440a
kavurilg <98037503+kavurilg@users.noreply.github.com>
2022-07-19 00:42:46 +0530

    Add files via upload

---
+++ CHOPRT.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b;
+	    scanf("%d%d",&a,&b);
+	    if(a==b)
+	    printf("=\n");
+	    else if(a<b)
+	    printf("<\n");
+	    else if(a>b)
+	    printf(">\n");
+	}
+	
+	return 0;
+}
+
